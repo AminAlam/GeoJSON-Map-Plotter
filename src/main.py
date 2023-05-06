4e8633d2bf965abbd38683a60fc089e379dbbd64
Amin Alam <ma.alamalhoda@gmail.com>
2023-05-06 23:03:59 +0200

    multiple files supported

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@ from configs import work_dir, click, np, plt, gpd, KMeans, Path
 import utils
 
 @click.command()
-@click.option('--geojson_file_path', default=Path.joinpath(work_dir, 'assets', 'geojsons', 'Iran.geojson') , help='Geojson file containing the coordinates of the states in Iran', required=True)
-@click.option('--coordinates_file_paths', help='List of coordinates file paths - should be .kml files', multiple=True, required=True,
+@click.option('--geojson_file_path', '-gfp', default=Path.joinpath(work_dir, 'assets', 'geojsons', 'Iran.geojson') , help='Geojson file containing the coordinates of the states in Iran', required=True)
+@click.option('--coordinates_file_paths', '-cfp', help='List of coordinates file paths - should be .kml files', multiple=True, required=True,
                default=[Path.joinpath(work_dir, 'assets', 'data', 'data1.kml'), Path.joinpath(work_dir, 'assets', 'data', 'data2.kml')])
-@click.option('--nums_clusters', help='List of number of clusters for each coordinates file. If zero, no clustering will be done for the corresponding coordinates file', multiple=True, required=True, default=[0, 0])
-@click.option('--colors', help='List of colors for each coordinates file.', multiple=True, required=True, default=['red', 'blue'])
-@click.option('--labels', help='List of labels for each coordinates file. If empty, no labels will be shown for the corresponding coordinates file', multiple=True, required=False, default=['data1', 'data2'])
+@click.option('--nums_clusters', '-nc', help='List of number of clusters for each coordinates file. If zero, no clustering will be done for the corresponding coordinates file', multiple=True, required=True, default=[0, 0])
+@click.option('--colors', '-c', help='List of colors for each coordinates file.', multiple=True, required=True, default=['red', 'blue'])
+@click.option('--labels', '-l', help='List of labels for each coordinates file. If empty, no labels will be shown for the corresponding coordinates file', multiple=True, required=False, default=None)
 @click.option('--title', default=None, help='Title of the map')
 @click.option('--save_path', default='map.png', help='Path to save the map - indicate the name of the file with extension. If None, the map will not be saved')
 def main(geojson_file_path, coordinates_file_paths, nums_clusters, colors, labels, title, save_path):
-    
+
     utils.check_lengths(coordinates_file_paths, nums_clusters, colors, labels)
 
     geojson_data = utils.import_geojson(geojson_file_path)
@@ -23,15 +23,16 @@ def main(geojson_file_path, coordinates_file_paths, nums_clusters, colors, label
         coordinates = utils.import_coords(coordinates_file_paths[i])
         num_clusters = nums_clusters[i]
         color = colors[i]
-        lebel = labels[i]
-        
+        if len(labels) != 0:
+            label = labels[i]
+        else:
+            label = None
+
         if num_clusters:
             kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(coordinates)
-            labels = kmeans.labels_
-
             radius_list = []
             for i in range(num_clusters):
-                cluster = coordinates[labels == i]
+                cluster = coordinates[kmeans.labels_ == i]
                 radius = cluster.shape[0]*60
                 radius_list.append(radius)
 
@@ -41,17 +42,17 @@ def main(geojson_file_path, coordinates_file_paths, nums_clusters, colors, label
             for i in range(num_clusters):
                 plt.scatter(kmeans.cluster_centers_[i, 0], kmeans.cluster_centers_[i, 1], s=radius_list[i], alpha=opacity_list[i], c=color, edgecolor=color) 
 
-        plt.scatter(coordinates[:, 0], coordinates[:, 1], s=5, alpha=1, edgecolor='none', c=color, label=lebel)
-
-    L = plt.legend(loc='lower left', fontsize=7, frameon=False)
-    plt.setp(L.texts, family='Times New Roman')
+        plt.scatter(coordinates[:, 0], coordinates[:, 1], s=5, alpha=1, edgecolor='none', c=color, label=label)
+    if len(labels) != 0:
+        L = plt.legend(loc='lower left', fontsize=7, frameon=False)
+        plt.setp(L.texts, family='Times New Roman')
 
     plt.tight_layout()
     plt.axis('off')
 
     if title:
         plt.title(title, fontsize=10, fontname='Times New Roman')
-        
+
     if save_path:
         plt.savefig(save_path, dpi=600, bbox_inches='tight')
     else:
